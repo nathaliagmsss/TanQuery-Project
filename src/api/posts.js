67bdbd1f00a17766ff22dd5a82045bc626c92a2d
Nathália Gomes <gmsssnathalia@gmail.com>
2025-08-16 13:51:42 -0300

    feat: adicionando tratamento de erros

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,18 +3,12 @@ import axios from "axios";
 // Função para buscar os dados na api
 
 export default fetchPost = async () => {
-    const response = await axios.get(
+    try {
+      const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
-    );
-    return response.data; //Retorna os dados
+      );
+      return response.data; //Retorna os dados
+    } catch (error) {
+      console.log("Dados não encontrados", error)
+    }
 }
-
-// Função para enviar um novo usuárioa
-
-export const createUser = async (newUser) => {
-    const response = await axios.post(
-      "https://jsonplaceholder.typicode.com/users",
-      newUser
-    );
-    return response.data; // Retorna o novo usuário
-  };
